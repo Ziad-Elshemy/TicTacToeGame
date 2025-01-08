5db9e80bf8e140e811ff0aaa5e3b1e8dc015c9d7
Ziad-Elshemy <zizoelshemy@gmail.com>
2025-01-08 21:08:51 +0200

    Update class name
    LoginScreenController

--- TicTacToeClient/src/tictactoeclient/LoginScreenController.java
+++ TicTacToeClient/src/tictactoeclient/LoginScreenController.java
@@ -26,7 +26,7 @@ import javafx.stage.Stage;
  *
  * @author youse
  */
-public class LoginController implements Initializable {
+public class LoginScreenController implements Initializable {
     
     Navigator navigator;
 
